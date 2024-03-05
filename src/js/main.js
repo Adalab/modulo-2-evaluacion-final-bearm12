e90dbb84d39977f4ce503c084c5a3438f5d3bb55
bearm12 <bearm12493@gmail.com>
2024-03-05 13:11:05 +0000

    favorites functionality

--- src/js/main.js
+++ src/js/main.js
@@ -2,37 +2,87 @@
 
 const searchInput = document.querySelector('.js-search-input');
 const searchButton = document.querySelector('.js-search-btn');
-const favSection = document.querySelector('.js-favs-container');
+const favSection = document.querySelector('.js-favs-section');
 const animeSection = document.querySelector('.js-search-container');
+const cardsContainer = document.querySelector('.js-cards-container');
+const favsContainer = document.querySelector('.js-favs-container')
 
 const URL_SERVER = `https://api.jikan.moe/v4/anime?q=`;
+const noImage = 'https://cdn.myanimelist.net/img/sp/icon/apple-touch-icon-256.png';
+const placeholderImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text='
 
 let animesList = [];
+let animeFavList = [];
+
+const renderFavoritesList = () => {
+    favsContainer.innerHTML = '';
+
+    for(const anime of animeFavList){
+        const favContainer = document.createElement('div');
+        favsContainer.appendChild(favContainer);
+
+        const favImage = document.createElement('img');
+        const favTilteEl = document.createElement('p');
+        const favTitleContent = document.createTextNode(anime.title)
+        const eliminateIcon = document.createElement('i');
+
+        favContainer.appendChild(favImage);
+        favImage.setAttribute('alt', anime.title)
+        if(anime.images.webp.samll_image_url !== noImage) {
+            favImage.setAttribute('src', anime.images.webp.small_image_url);
+        } else {
+            favImage.setAttribute('src', `${placeholderImage}${anime.title}`);   
+        }
+
+        favContainer.appendChild(favTilteEl);
+        favTilteEl.appendChild(favTitleContent);
+        favContainer.appendChild(eliminateIcon);
+        eliminateIcon.classList.add('fa-regular', 'fa-circle-xmark');
+    }
+}
+
+const handleFavorite = (event) => {
+    const clickedCard = event.currentTarget;
+    
+    const favAnime = animesList.find(anime => parseInt(clickedCard.id) === anime.mal_id);
+    const favIndex = animeFavList.findIndex(favItem => parseInt(clickedCard.id) === favItem.mal_id);
+
+    if (favIndex === -1) {
+        animeFavList.push(favAnime)
+    }
+    console.log(animeFavList)
+    renderFavoritesList();    
+}
 
 const renderAnimeList = () => {
-    const titleElementResults = document.createElement('h2');
-    const titleContentResults = document.createTextNode('Resultados');
-    animeSection.appendChild(titleElementResults);
-    titleElementResults.appendChild(titleContentResults);
-
-    const animeCardContainer = document.createElement('div');
-    animeSection.appendChild(animeCardContainer);
-
+    cardsContainer.innerHTML = '';
+    
     for(const anime of animesList){
         const animeCard = document.createElement('div');
-        animeCardContainer.appendChild(animeCard);
+        cardsContainer.appendChild(animeCard);
+        animeCard.setAttribute('id', anime.mal_id);
+        animeCard.classList.add('js-anime-card')
 
         const animeImage = document.createElement('img');
         animeCard.appendChild(animeImage);
-        animeImage.setAttribute('src', anime.images.webp.image_url);
-        animeImage.setAttribute('alt', anime.title_english);
+        animeImage.setAttribute('alt', anime.title);
 
+        if(anime.images.webp.image_url !== noImage) {
+            animeImage.setAttribute('src', anime.images.webp.image_url);
+        } else {
+            animeImage.setAttribute('src', `${placeholderImage}${anime.title}`);   
+        }
+        
         const animeTitleElement = document.createElement('p');
-        const animeTitle = document.createTextNode(anime.title_english);
+        const animeTitle = document.createTextNode(anime.title);
         animeCard.appendChild(animeTitleElement);
         animeCard.appendChild(animeTitle);
-    }    
-
+    }
+    
+    const cards = document.querySelectorAll('.js-anime-card');
+    for(const card of cards) {
+        card.addEventListener('click', handleFavorite);
+    }
 }
 
 const handleSearch = (event) => {
@@ -44,7 +94,6 @@ const handleSearch = (event) => {
         .then(response => response.json())
         .then((data) => {
             animesList = data.data;
-            console.log(animesList)
             renderAnimeList();
         })
 }
